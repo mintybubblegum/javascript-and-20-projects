f946332a0e46829e860119cdb77f83f8be38b222
Noa Trujillo Zamora <atrujilz@emeal.nttdata.com>
2023-09-20 13:21:21 +0200

    finished variables section

---
+++ 02-Variables/js/01-app.js
@@ -0,0 +1,22 @@
+//Inicializar una variable con un valor
+var producto = "24 inches monitor";
+console.log(producto);
+
+//Las variables se pueden reasignar
+producto = '55 inches TV';
+console.log(producto);
+
+//JS es un lenguaje de tipo dinámico, no se especifica tipo de dato - no es tipado
+producto = 20;
+console.log(producto);
+
+//Inicializar múltiples variables
+var categoria = 'Computadoras',
+    marca = 'DELL',
+    calificacion = 5;
+
+// Estilos para nombrar variables con más de una palabra
+var  nombreProducto = 'Monitor 30 Pulgadas'; // CamelCase
+var nombre_producto = 'Monitor 30 Pulgadas'; //underscore
+var NombreProducto = 'Monitor 30 Pulgadas'; // pascal case
+var nombreproducto = 'Monitor 30 Pulgadas';
---
+++ 02-Variables/js/03-app.js
@@ -0,0 +1,17 @@
+//!las variables const no se pueden reasignar ni se pueden iniciar sin un valor
+
+// Veamos el 3er tipo de formas de crear variables, const
+// Existen pocas diferencias entre let y const, asi que vamos a verlas:
+
+// primero una variable con const su valor no puede ser re-asignado:
+
+const producto = 'Tablet';
+console.log(producto);
+producto = 'Monitor 23 Pulgadas';
+
+// Por otro lado, las variables con const, deben iniciarse con un valor:
+const precio;
+precio = 20;
+console.log(precio);
+
+// Existen otras diferencias entre const y let especialmente cuando se trabajan con arreglos y objetos, que te mostraré mas adelante, pero recuerda, las variables con const, no se pueden re-asignar y tampoco pueden iniciar sin un valor.
