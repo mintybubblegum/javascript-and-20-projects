32a6af6c5f83da1767b9cbe557b24a75383fc8b2
Noa Trujillo Zamora <atrujilz@emeal.nttdata.com>
2023-09-20 12:50:20 +0200

    files structure and introduction module finished

---
+++ 01-Introduccion/js/app.js
@@ -0,0 +1,6 @@
+const nombre = prompt('Whats your name?');
+
+//Toma el nombre y lo muestra en pantalla
+document.querySelector('.contenido').innerHTML = `${nombre} está aprendiendo JS`;
+
+const product = '24 inches monitor';
